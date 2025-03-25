bf62d66266626d14c642a3a1084ef2495eddfce3
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-25 10:19:25 +0300

    fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // Your code here
+fetch("https://json-chi-six.vercel.app/characters/")
+.then(response=>response.json )
+.then( data => console.log(data))
+.catch(error=>console.log(error))
+console.log();
+
