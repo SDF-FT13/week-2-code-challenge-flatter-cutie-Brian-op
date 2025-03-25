344619783818cfe98bb5c9343e290c8c6a0c0373
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-25 10:30:05 +0300

    getting characters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // Your code here
+function getingCharacters() {
 fetch("https://json-chi-six.vercel.app/characters/")
 .then(response=>response.json )
-.then( data => console.log(data))
-.catch(error=>console.log(error))
-console.log();
+.then( data => {
+    const characterBar= document.getElementById("character-bar")
+    data.forEach(character=>{const span =document.getElementById("span");
+        span.textContent = character.name;
+        span.addEventListener("click",()=>showCharacterDetails(character));
+        characterBar.appendChild(span);            
+    })
+})    
+}
 
